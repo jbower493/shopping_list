5e7a114dc4bf44b1fb2ac4d278e510c31bfe6eb1
Jamie Bower <j_bower@hotmail.co.uk>
2022-08-14 21:42:54 +1000

    feat: add extra fake tests

--- cypress/e2e/app/app.cy.ts
+++ cypress/e2e/app/app.cy.ts
@@ -1,7 +1,6 @@
-// [SPL-1572,SPL-1126,SPL-1426]
-
 describe('Visit the app', () => {
-    it('Can load the login page.', () => {
+    // [SPL-1572,SPL-1126,SPL-1426]
+    it('Successfully loads the app.', () => {
         cy.visit('http://localhost:3000')
     })
 })
