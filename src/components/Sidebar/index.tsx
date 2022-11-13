07d93ba6685fbe548041d19bbba963ab6873026f
Jamie Bower <j_bower@hotmail.co.uk>
2022-11-13 12:27:36 +1100

    style: logout button as actual button

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,31 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useGetUserQuery, useLogoutMutation } from 'utils/api/auth'
-import Loader from 'components/Loader'
 import { toast } from 'react-hot-toast'
+import Button from 'components/Button'
 
 function Sidebar({ showMenu, setShowMenu }: { showMenu: boolean; setShowMenu: (show: boolean) => void }) {
     const { data, isFetching, isError } = useGetUserQuery()
     const [logout, { isLoading: isLogoutLoading }] = useLogoutMutation()
-
-    const renderLogoutButton = () =>
-        isLogoutLoading ? (
-            <Loader size='small' />
-        ) : (
-            <button
-                type='button'
-                onClick={() => {
-                    logout()
-                        .unwrap()
-                        .then((result) => {
-                            toast.success(result.message)
-                            setShowMenu(false)
-                        })
-                }}
-            >
-                Logout
-            </button>
-        )
 
     return (
         <nav className={`fixed w-40 h-screen pt-14 bg-gray-100 flex${showMenu ? '' : ' hidden'} sm:flex flex-col justify-between`}>
@@ -51,7 +32,28 @@ function Sidebar({ showMenu, setShowMenu }: { showMenu: boolean; setShowMenu: (s
                     </Link>
                 </li>
             </ul>
-            {!isFetching && !isError && data ? <div className='flex justify-center items p-4'>{renderLogoutButton()}</div> : ''}
+            {!isFetching && !isError && data ? (
+                <div className='flex justify-center items p-4'>
+                    <Button
+                        className='w-full'
+                        color='secondary'
+                        loading={isLogoutLoading}
+                        disabled={isLogoutLoading}
+                        onClick={() => {
+                            logout()
+                                .unwrap()
+                                .then((result) => {
+                                    toast.success(result.message)
+                                    setShowMenu(false)
+                                })
+                        }}
+                    >
+                        Logout
+                    </Button>
+                </div>
+            ) : (
+                ''
+            )}
         </nav>
     )
 }
