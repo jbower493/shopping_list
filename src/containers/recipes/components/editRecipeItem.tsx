6871c784bca793139b2d374112581e1c5c3f5155
Jamie Bower <j_bower@hotmail.co.uk>
2024-04-11 20:47:34 +1000

    fix[recipe]: loader incorrectly showing on next index item when previous item is deleting optimistically

--- src/containers/recipes/components/editRecipeItem.tsx
+++ src/containers/recipes/components/editRecipeItem.tsx
@@ -1,6 +1,5 @@
 import { singleRecipeQueryKey, useRemoveItemFromRecipeMutation } from '../queries'
 import { TrashIcon } from '@heroicons/react/24/solid'
-import Loader from 'components/Loader'
 import { queryClient } from 'utils/queryClient'
 import { RecipeItem } from '../types'
 import ItemWithQuantity from 'components/ItemWithQuantity'
@@ -11,30 +10,26 @@ interface EditRecipeItemProps {
 }
 
 function EditRecipeItem({ item: { name, id, item_quantity }, recipeId }: EditRecipeItemProps) {
-    const { mutate: removeItemFromRecipe, isLoading: isRemoveItemFromRecipeLoading } = useRemoveItemFromRecipeMutation()
+    const { mutate: removeItemFromRecipe } = useRemoveItemFromRecipeMutation()
 
     return (
         <li className='flex justify-between w-full max-w-md mb-2'>
             <ItemWithQuantity quantityValue={item_quantity.quantity} unitSymbol={item_quantity.quantity_unit?.symbol} itemName={name} />
-            {isRemoveItemFromRecipeLoading ? (
-                <Loader size='small' />
-            ) : (
-                <button
-                    type='button'
-                    onClick={() => {
-                        removeItemFromRecipe(
-                            { recipeId: recipeId.toString(), itemId: id },
-                            {
-                                onSuccess: () => {
-                                    queryClient.invalidateQueries(singleRecipeQueryKey(recipeId.toString()))
-                                }
+            <button
+                type='button'
+                onClick={() => {
+                    removeItemFromRecipe(
+                        { recipeId: recipeId.toString(), itemId: id },
+                        {
+                            onSuccess: () => {
+                                queryClient.invalidateQueries(singleRecipeQueryKey(recipeId.toString()))
                             }
-                        )
-                    }}
-                >
-                    <TrashIcon className='w-5 text-primary hover:text-primary-hover' />
-                </button>
-            )}
+                        }
+                    )
+                }}
+            >
+                <TrashIcon className='w-5 text-primary hover:text-primary-hover' />
+            </button>
         </li>
     )
 }
