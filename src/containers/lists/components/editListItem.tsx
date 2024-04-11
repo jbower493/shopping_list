d24f9580fab64b49fe28cf9598d73aaf49b2ea7d
Jamie Bower <j_bower@hotmail.co.uk>
2024-04-11 21:27:09 +1000

    feat: animate list and recipe items being deleted

--- src/containers/lists/components/editListItem.tsx
+++ src/containers/lists/components/editListItem.tsx
@@ -2,6 +2,9 @@ import { useRemoveItemFromListMutation } from '../queries'
 import { TrashIcon } from '@heroicons/react/24/solid'
 import { ListItem } from '../types'
 import ItemWithQuantity from 'components/ItemWithQuantity'
+import { useState } from 'react'
+
+import './removing.css'
 
 interface EditListItemProps {
     item: ListItem
@@ -9,18 +12,28 @@ interface EditListItemProps {
 }
 
 function EditListItem({ item: { name, id, item_quantity }, listId }: EditListItemProps) {
+    const [isBeingRemoved, setIsBeingRemoved] = useState(false)
+
     const { mutate: removeItemFromList } = useRemoveItemFromListMutation()
 
+    function removeItem() {
+        removeItemFromList({ listId: listId.toString(), itemId: id })
+    }
+
     return (
-        <li className='flex justify-between w-full max-w-md mb-2'>
+        <li className={`flex justify-between w-full max-w-md mb-2 rounded-md relative top-0 left-0 h-6${isBeingRemoved ? ' removing' : ''}`}>
             <ItemWithQuantity quantityValue={item_quantity.quantity} unitSymbol={item_quantity.quantity_unit?.symbol} itemName={name} />
             <button
                 type='button'
                 onClick={() => {
-                    removeItemFromList({ listId: listId.toString(), itemId: id })
+                    setIsBeingRemoved(true)
+                    setTimeout(() => {
+                        setIsBeingRemoved(false)
+                        removeItem()
+                    }, 300)
                 }}
             >
-                <TrashIcon className='w-5 text-primary hover:text-primary-hover' />
+                <TrashIcon className={`w-5 text-primary hover:text-primary-hover${isBeingRemoved ? ' hidden' : ''}`} />
             </button>
         </li>
     )
--- src/containers/recipes/components/editRecipeItem.tsx
+++ src/containers/recipes/components/editRecipeItem.tsx
@@ -3,6 +3,7 @@ import { TrashIcon } from '@heroicons/react/24/solid'
 import { queryClient } from 'utils/queryClient'
 import { RecipeItem } from '../types'
 import ItemWithQuantity from 'components/ItemWithQuantity'
+import { useState } from 'react'
 
 interface EditRecipeItemProps {
     item: RecipeItem
@@ -10,25 +11,28 @@ interface EditRecipeItemProps {
 }
 
 function EditRecipeItem({ item: { name, id, item_quantity }, recipeId }: EditRecipeItemProps) {
+    const [isBeingRemoved, setIsBeingRemoved] = useState(false)
+
     const { mutate: removeItemFromRecipe } = useRemoveItemFromRecipeMutation()
 
+    function removeItem() {
+        removeItemFromRecipe({ recipeId: recipeId.toString(), itemId: id })
+    }
+
     return (
-        <li className='flex justify-between w-full max-w-md mb-2'>
+        <li className={`flex justify-between w-full max-w-md mb-2 rounded-md relative top-0 left-0 h-6${isBeingRemoved ? ' removing' : ''}`}>
             <ItemWithQuantity quantityValue={item_quantity.quantity} unitSymbol={item_quantity.quantity_unit?.symbol} itemName={name} />
             <button
                 type='button'
                 onClick={() => {
-                    removeItemFromRecipe(
-                        { recipeId: recipeId.toString(), itemId: id },
-                        {
-                            onSuccess: () => {
-                                queryClient.invalidateQueries(singleRecipeQueryKey(recipeId.toString()))
-                            }
-                        }
-                    )
+                    setIsBeingRemoved(true)
+                    setTimeout(() => {
+                        setIsBeingRemoved(false)
+                        removeItem()
+                    }, 300)
                 }}
             >
-                <TrashIcon className='w-5 text-primary hover:text-primary-hover' />
+                <TrashIcon className={`w-5 text-primary hover:text-primary-hover${isBeingRemoved ? ' hidden' : ''}`} />
             </button>
         </li>
     )
